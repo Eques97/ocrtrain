a2e62d2ec395144d2f313633df2f709acb5b572d
Eques97 <heinhtetthuta@gmail.com>
2022-06-14 14:37:38 +0630

    replace ctc_loss with ctc_batch_cost

--- train/cost.py
+++ train/cost.py
@@ -1,17 +1,14 @@
 from tensorflow import constant
-from tensorflow.nn import ctc_loss
+from tensorflow.keras.backend import ctc_batch_cost
 
 from train import BATCH_SIZE, FRAME_SIZE
 
-def ctcloss(labels, logits):
-    label_length = constant(FRAME_SIZE, shape=(BATCH_SIZE,))
-    logit_length = constant(FRAME_SIZE, shape=(BATCH_SIZE,))
-    loss = ctc_loss(
-        labels,
-        logits,
-        label_length=label_length,
-        logit_length=logit_length,
-        logits_time_major=False,
-        blank_index=None,
-    )
-    return loss
+def ctcloss(y_true, y_pred):
+    input_length = constant(FRAME_SIZE, shape = (BATCH_SIZE, 1))
+    label_length = constant(FRAME_SIZE, shape = (BATCH_SIZE, 1))
+    loss_batch = ctc_batch_cost(
+        y_true = y_true,
+        y_pred = y_pred,
+        input_length = input_length,
+        label_length = label_length)
+    return loss_batch
